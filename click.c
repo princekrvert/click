af0bbc472ffb9614a5e0e68a8c7b2a7c90e90bb5
Princekrvert <princekrvert@gmail.com>
2023-07-13 16:09:00 +0530

    first commit

---
+++ click.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+// NOw show the menu for the pkg to be installed 
+printf("\033[31;1m These pkg will be installed \n");
+printf("\033[35;1m[01] \033[32;1mpython3\n");
+printf("\033[35;1m[02] \033[32;1mpython2\n");
+printf("\033[35;1m[03] \033[32;1mNodejs\n");
+printf("\033[35;1m[04] \033[32;1mGolang\n");
+printf("\033[35;1m[05] \033[32;1mRust\n");
+printf("\033[35;1m[06] \033[32;1mPhp\n");
+printf("\033[35;1m[07] \033[32;1mProot\n");
+printf("\033[35;1m[08] \033[32;1mCowsay\n");
+printf("\033[35;1m[09] \033[32;1mCmatrix\n");
+printf("\033[35;1m[10] \033[32;1mPerl\n");
+printf("\033[35;1m[11] \033[32;1mNmap\n");
+printf("\033[35;1m[12] \033[32;1mCurl\n");
+printf("\033[35;1m[13] \033[32;1mWget\n");
+printf("\033[35;1m[14] \033[32;1mUnzip\n");
+printf("\033[35;1m[15] \033[32;1mtar\n");
+printf("\033[35;1m[16] \033[32;1mdirb\n");
+
+
+
+}
